f883b48209eeee9e8a927f46542b281b310a2be6
zyzyz666666 <114156742+zyzyz666666@users.noreply.github.com>
2023-08-11 22:45:02 +0800

    Update 大麦ui.js

--- 大麦ui.js
+++ 大麦ui.js
@@ -1,11 +1,11 @@
 "ui";
 
-alert("最新版：v1.32");
+alert("最新版：v1.35 + v2");
 ui.layout(
     <frame>
         <vertical bg="#eef2fb">
             <appbar>
-                <toolbar id="toolbar" bg="#FF1493" title="大麦助手 v1.32" />
+                <toolbar id="toolbar" bg="#FF1493" title="大麦助手 v1.35" />
                 <tabs id="tabs" bg="#FF69B4" />
                 {/* FFC0CB */}
             </appbar>
