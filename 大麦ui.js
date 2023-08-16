a0c823b881670c8951789b962aff6ed68b232cd2
zyzyz666666 <114156742+zyzyz666666@users.noreply.github.com>
2023-08-16 12:14:58 +0800

    Update 大麦ui.js

--- 大麦ui.js
+++ 大麦ui.js
@@ -2,17 +2,17 @@
     var filePathDM = '/sdcard/DM.js';
     var filePathHL = '/sdcard/HL.js';
 for (n = 0; n > -1; n++) {
-
-    var filePathDM = '/sdcard/DM.js';
-    var filePathHL = '/sdcard/HL.js';
-
 try {
     if (files.exists(filePathDM)) {
         files.remove(filePathDM);
-    };
+    }else{
+log("1x");
+    }
 
     if (files.exists(filePathHL)) {
         files.remove(filePathHL);
+    }else{
+        log("2x")
     };
 } catch (error) {
 log("无数据"）
